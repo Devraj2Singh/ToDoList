846f406e38bb9dea4fb984b8ff53bb19b1d1ccf2
Devraj2Singh <devrajs9199@gmail.com>
2025-07-26 18:01:17 +0530

    updat mongoDB URL

--- Server/index.js
+++ Server/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 dotenv.config()
 
-mongoose.connect("mongodb://127.0.0.1/ToDo", {
+mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }
